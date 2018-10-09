9b4a222928f893290bf9d1eb294a0a0d343ac803
github username <github email address>
2018-10-08 23:06:25 -0400

    solving lab

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+require 'pry'
 
+def sort_array_asc (array)
+   array.sort
+end
+
+def sort_array_desc (array)
+  array.sort {|x,y| y <=> x}
+end
+
+
+def sort_array_char_count (array)
+  array.sort {|x,y| x.size <=> y.size}
+end
+
+
+def swap_elements (array)
+  second_element = array[1]
+  array[1] = array[2]
+  array[2] = second_element
+  array
+
+end
+
+def reverse_array (array)
+array.reverse
+end
+
+def kesha_maker (array)
+  array.each {|element| element[2] = "$" }
+  array
+end
+
+def find_a (array)
+ array.select {|element|
+ element[0] == "a" }
+end
+
+def sum_array (array)
+sum = 0
+array.map {|x| sum += x}
+end
+
+def add_s (array)
+new_array = array.selects {|word| !array.size == 1
+}
+new_array.map {|word| word += "s"}
+binding.pry
+end
